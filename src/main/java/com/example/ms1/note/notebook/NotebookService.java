6b7c2d2fb3908e07e6fc07276023db7f75adb026
kimyeji <kimyeji546408@gmail.com>
2024-05-13 15:02:49 +0900

    start

---
+++ src/main/java/com/example/ms1/note/notebook/NotebookService.java
@@ -0,0 +1,24 @@
+package com.example.ms1.note.notebook;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class NotebookService {
+    private final NotebookRepository notebookRepository;
+
+    public Notebook getNotebook(Long notebookId) {
+        return notebookRepository.findById(notebookId).orElseThrow();
+    }
+
+    public List<Notebook> getNotebookList() {
+        return notebookRepository.findAll();
+    }
+
+    public Notebook save(Notebook notebook) {
+        return notebookRepository.save(notebook);
+    }
+}
